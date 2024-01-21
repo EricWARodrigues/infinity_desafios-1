cf4432b01316fe923a06319908e76660cea46245
EricWARodrigues <128868563+EricWARodrigues@users.noreply.github.com>
2024-01-21 13:18:18 -0300

    Update exercicios.py

--- exercicios.py
+++ exercicios.py
@@ -1,8 +1,23 @@
+def calcular(num1, num2, operador):
+    if operador == '+':
+        return num1 + num2
+    elif operador == '-':
+        return num1 - num2
+    elif operador == '*':
+        return num1 * num2
+    elif operador == '/':
+        if num2 == 0:
+            return 'Impossível dividir por 0'
+        else:
+            return num1 / num2
+    else:
+        return 'Operação inválida'
+
 print('-' * 30)
 print('Bem vindo a calculadora simples')
 print('-' * 30)
 
-num1 = input('informe um numero: ')
+num1 = int(input('informe um numero: '))
 operador= input('informe o operador(+,-,*,/): ')
-num2 = input('informe um numero: ')
+num2 = int(input('informe um numero: '))
 print(f'o resultado é {calcular(num1, num2, operador)}')
