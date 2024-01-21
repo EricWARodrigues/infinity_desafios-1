418568a61deba584536b0658688c91817516a8de
EricWARodrigues <128868563+EricWARodrigues@users.noreply.github.com>
2024-01-21 13:17:18 -0300

    Update exercicios.py
    Exercício realizado a duas mãos, uma parte para cada um dos operadores.

--- exercicios.py
+++ exercicios.py
@@ -1,3 +1,18 @@
+def calcular(num1, num2, operador):
+    if operador == '+':
+        return num1 + num2
+    elif operador == '-':
+        return num1 - num2
+    elif operador == '*':
+        return num1 * num2
+    elif operador == '/':
+        if num2 == 0:
+            return 'Impossível dividir por 0'
+        else:
+            return num1 / num2
+    else:
+        return 'Operação inválida'
+
 print('-' * 30)
 print('Bem vindo a calculadora simples')
 print('-' * 30)
