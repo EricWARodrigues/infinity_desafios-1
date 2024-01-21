f93a7292b18f82df0875dbfdc2d22f88d74f8b13
Boydatitia <90669688+Boydatitia@users.noreply.github.com>
2024-01-21 13:15:03 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,8 @@
+print('-' * 30)
+print('Bem vindo a calculadora simples')
+print('-' * 30)
+
+num1 = input('informe um numero: ')
+operador= input('informe o operador(+,-,*,/): ')
+num2 = input('informe um numero: ')
+print(f'o resultado é {calcular(num1, num2, operador)}')
